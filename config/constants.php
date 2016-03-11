ff7540b09cba6380f18aba9cc9dd48d324029bc3
Stephen Foo <stephen.frx@gmail.com>
2016-03-11 11:36:01 +0800

    add constants

---
+++ config/constants.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 常用系统参数
+ *
+ * @author FuRongxin
+ * @date 2016-03-11
+ * @version 2.0
+ */
+return [
+
+	/**
+	 * 数据状态代码
+	 */
+	'status' => [
+		'enable'  => true,
+		'disable' => false,
+	],
+];
