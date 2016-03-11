cb2ea2c7b74d593c151391bcd6c47a9de1337397
Stephen Foo <stephen.frx@gmail.com>
2016-03-11 11:32:20 +0800

    Update plugins settings

--- config/database.php
+++ config/database.php
@@ -2,47 +2,47 @@
 
 return [
 
-	/*
-		    |--------------------------------------------------------------------------
-		    | PDO Fetch Style
-		    |--------------------------------------------------------------------------
-		    |
-		    | By default, database results will be returned as instances of the PHP
-		    | stdClass object; however, you may desire to retrieve records in an
-		    | array format for simplicity. Here you can tweak the fetch style.
-		    |
-	*/
+	/**
+	|--------------------------------------------------------------------------
+	| PDO Fetch Style
+	|--------------------------------------------------------------------------
+	|
+	| By default, database results will be returned as instances of the PHP
+	| stdClass object; however, you may desire to retrieve records in an
+	| array format for simplicity. Here you can tweak the fetch style.
+	|
+	 */
 
 	'fetch'       => PDO::FETCH_CLASS,
 
-	/*
-		    |--------------------------------------------------------------------------
-		    | Default Database Connection Name
-		    |--------------------------------------------------------------------------
-		    |
-		    | Here you may specify which of the database connections below you wish
-		    | to use as your default connection for all database work. Of course
-		    | you may use many connections at once using the Database library.
-		    |
-	*/
+	/**
+	|--------------------------------------------------------------------------
+	| Default Database Connection Name
+	|--------------------------------------------------------------------------
+	|
+	| Here you may specify which of the database connections below you wish
+	| to use as your default connection for all database work. Of course
+	| you may use many connections at once using the Database library.
+	|
+	 */
 
 	'default'     => env('DB_CONNECTION', 'pgsql'),
 
-	/*
-		    |--------------------------------------------------------------------------
-		    | Database Connections
-		    |--------------------------------------------------------------------------
-		    |
-		    | Here are each of the database connections setup for your application.
-		    | Of course, examples of configuring each database platform that is
-		    | supported by Laravel is shown below to make development simple.
-		    |
-		    |
-		    | All database work in Laravel is done through the PHP PDO facilities
-		    | so make sure you have the driver for your particular database of
-		    | choice installed on your machine before you begin development.
-		    |
-	*/
+	/**
+	|--------------------------------------------------------------------------
+	| Database Connections
+	|--------------------------------------------------------------------------
+	|
+	| Here are each of the database connections setup for your application.
+	| Of course, examples of configuring each database platform that is
+	| supported by Laravel is shown below to make development simple.
+	|
+	|
+	| All database work in Laravel is done through the PHP PDO facilities
+	| so make sure you have the driver for your particular database of
+	| choice installed on your machine before you begin development.
+	|
+	 */
 
 	'connections' => [
 
@@ -66,12 +66,12 @@ return [
 
 		'pgsql'  => [
 			'driver'   => 'pgsql',
-			'host'     => env('DB_HOST', 'localhost'),
-			'database' => env('DB_DATABASE', 'forge'),
-			'username' => env('DB_USERNAME', 'forge'),
-			'password' => env('DB_PASSWORD', ''),
+			'host'     => env('DB_HOST', '202.193.171.253'),
+			'database' => env('DB_DATABASE', 'abc'),
+			'username' => env('DB_USERNAME', 'jwxt'),
+			'password' => env('DB_PASSWORD', 'jwxt..'),
 			'charset'  => 'utf8',
-			'prefix'   => '',
+			'prefix'   => 't_',
 			'schema'   => 'public',
 		],
 
@@ -87,29 +87,29 @@ return [
 
 	],
 
-	/*
-		    |--------------------------------------------------------------------------
-		    | Migration Repository Table
-		    |--------------------------------------------------------------------------
-		    |
-		    | This table keeps track of all the migrations that have already run for
-		    | your application. Using this information, we can determine which of
-		    | the migrations on disk haven't actually been run in the database.
-		    |
-	*/
+	/**
+	|--------------------------------------------------------------------------
+	| Migration Repository Table
+	|--------------------------------------------------------------------------
+	|
+	| This table keeps track of all the migrations that have already run for
+	| your application. Using this information, we can determine which of
+	| the migrations on disk haven't actually been run in the database.
+	|
+	 */
 
 	'migrations'  => 'migrations',
 
-	/*
-		    |--------------------------------------------------------------------------
-		    | Redis Databases
-		    |--------------------------------------------------------------------------
-		    |
-		    | Redis is an open source, fast, and advanced key-value store that also
-		    | provides a richer set of commands than a typical key-value systems
-		    | such as APC or Memcached. Laravel makes it easy to dig right in.
-		    |
-	*/
+	/**
+	|--------------------------------------------------------------------------
+	| Redis Databases
+	|--------------------------------------------------------------------------
+	|
+	| Redis is an open source, fast, and advanced key-value store that also
+	| provides a richer set of commands than a typical key-value systems
+	| such as APC or Memcached. Laravel makes it easy to dig right in.
+	|
+	 */
 
 	'redis'       => [
 
