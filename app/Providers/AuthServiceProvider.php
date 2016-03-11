32218ea19c5c21577872c5686d551f8837184c09
Stephen Foo <stephen.frx@gmail.com>
2016-03-11 11:50:59 +0800

    add auth user provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,27 +5,28 @@ namespace App\Providers;
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
-class AuthServiceProvider extends ServiceProvider
-{
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
-    protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
-    ];
+class AuthServiceProvider extends ServiceProvider {
 
-    /**
-     * Register any application authentication / authorization services.
-     *
-     * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
-     * @return void
-     */
-    public function boot(GateContract $gate)
-    {
-        $this->registerPolicies($gate);
+	/**
+	 * The policy mappings for the application.
+	 *
+	 * @var array
+	 */
+	protected $policies = [
+		'App\Model' => 'App\Policies\ModelPolicy',
+	];
 
-        //
-    }
+	/**
+	 * Register any application authentication / authorization services.
+	 *
+	 * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
+	 * @return void
+	 */
+	public function boot(GateContract $gate) {
+		$this->registerPolicies($gate);
+
+		Auth::provider('dean', function ($app, array $config) {
+			return new DeanUserProvider($config['model']);
+		});
+	}
 }
