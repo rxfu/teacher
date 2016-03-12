b5e114043b06fb9dc2ed146334ac3175c2575590
Stephen <stephen.frx@gmail.com>
2016-03-12 12:55:43 +0800

    add auth login and password change

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,71 +2,96 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
-use Validator;
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\ThrottlesLogins;
+use App\Models\User;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Validator;
 
-class AuthController extends Controller
-{
-    /*
-    |--------------------------------------------------------------------------
-    | Registration & Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users, as well as the
-    | authentication of existing users. By default, this controller uses
-    | a simple trait to add these behaviors. Why don't you explore it?
-    |
-    */
+class AuthController extends Controller {
 
-    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+	/**
+	|--------------------------------------------------------------------------
+	| Registration & Login Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handles the registration of new users, as well as the
+	| authentication of existing users. By default, this controller uses
+	| a simple trait to add these behaviors. Why don't you explore it?
+	|
+	 */
 
-    /**
-     * Where to redirect users after login / registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/';
+	use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
-    /**
-     * Create a new authentication controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest', ['except' => 'logout']);
-    }
+	/**
+	 * Where to redirect users after login / registration.
+	 *
+	 * @var string
+	 */
+	protected $redirectTo = '/';
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|confirmed|min:6',
-        ]);
-    }
+	protected $username = 'username';
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
-    }
+	/**
+	 * Create a new authentication controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		$this->middleware('guest', ['except' => 'logout']);
+	}
+
+	/**
+	 * Get a validator for an incoming registration request.
+	 *
+	 * @param  array  $data
+	 * @return \Illuminate\Contracts\Validation\Validator
+	 */
+	protected function validator(array $data) {
+		return Validator::make($data, [
+			'username' => 'required|unique:pk_js',
+			'password' => 'required|confirmed|min:6',
+		]);
+	}
+
+	/**
+	 * Create a new user instance after a valid registration.
+	 *
+	 * @param  array  $data
+	 * @return User
+	 */
+	protected function create(array $data) {
+		return User::create([
+			'username' => $data['username'],
+			'password' => bcrypt($data['password']),
+		]);
+	}
+
+	/**
+	 * Get the failed login response instance.
+	 *
+	 * @param \Illuminate\Http\Request  $response
+	 * @return \Illuminate\Http\Response
+	 */
+	protected function sendFailedLoginResponse(Request $request) {
+		return redirect()->back()
+			->withInput($request->only($this->loginUsername()))
+			->withErrors([
+				$this->loginUsername() => $this->getFailedLoginMessage(),
+			]);
+	}
+
+	/**
+	 * Get the needed authorization credentials from the request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array
+	 */
+	protected function getCredentials(Request $request) {
+		return [
+			'jsgh' => $request->get($this->loginUsername()),
+			'mm'   => $request->get('password'),
+		];
+	}
+
 }
--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -3,30 +3,73 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Auth;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
-class PasswordController extends Controller
-{
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
-    */
+class PasswordController extends Controller {
 
-    use ResetsPasswords;
+	/**
+	|--------------------------------------------------------------------------
+	| Password Reset Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller is responsible for handling password reset requests
+	| and uses a simple trait to include this behavior. You're free to
+	| explore this trait and override any methods you wish to tweak.
+	|
+	 */
 
-    /**
-     * Create a new password controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
+	use ResetsPasswords;
+
+	/**
+	 * Create a new password controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		$this->middleware('guest', ['except' => ['showChangeForm', 'change']]);
+	}
+
+	/**
+	 * 显示修改密码表单
+	 * @author FuRongxin
+	 * @date    2016-01-12
+	 * @version 2.0
+	 * @return  \Illuminate\Http\Response 修改密码表单
+	 */
+	public function showChangeForm() {
+		return view('auth.change')->withTitle('修改密码');
+	}
+
+	/**
+	 * 修改密码
+	 * @author FuRongxin
+	 * @date    2016-01-12
+	 * @version 2.0
+	 * @param   \Illuminate\Http\Request $request 表单请求
+	 * @return  \Illuminate\Http\Response  修改密码表单
+	 */
+	public function change(Request $request) {
+		$this->validate($request, [
+			'old_password'          => 'required',
+			'password'              => 'required|min:6|confirmed',
+			'password_confirmation' => 'required|min:6',
+		]);
+
+		list($old, $password, $confirm) = array_values($request->only('old_password', 'password', 'password_confirmation'));
+
+		$user = Auth::user();
+		if (Auth::attempt(['jsgh' => $user->jsgh, 'mm' => $old])) {
+			if ($password === $confirm && mb_strlen($password) >= 6) {
+				$user->mm = $password;
+				$user->save();
+
+				return redirect('password/change')->withStatus('修改密码成功');
+			}
+		}
+
+		return redirect()->back()
+			->withInput($request->only('old_password'))
+			->withErrors(['old_password' => '修改密码失败']);
+	}
 }
