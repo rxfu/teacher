9124e52475f705c72d5f47737315650135a8a2ec
Stephen Foo <stephen.frx@gmail.com>
2016-03-13 18:25:50 +0800

    add major course and timetable

---
+++ app/Models/Mjcourse.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * 专业课程信息
+ *
+ * @author FuRongxin
+ * @date 2016-03-13
+ * @version 2.0
+ */
+class Mjcourse extends Model {
+
+	protected $table = 'pk_kczy';
+
+	protected $primaryKey = 'kcxh';
+
+	public $incrementing = false;
+
+	public $timestamps = false;
+
+	/**
+	 * 课程平台
+	 * @author FuRongxin
+	 * @date    2016-03-13
+	 * @version 2.0
+	 * @return  object 所属对象
+	 */
+	public function platform() {
+		return $this->belongsTo('App\Models\Platform', 'pt', 'dm');
+	}
+
+	/**
+	 * 课程性质
+	 * @author FuRongxin
+	 * @date    2016-03-13
+	 * @version 2.0
+	 * @return  object 所属对象
+	 */
+	public function property() {
+		return $this->belongsTo('App\Models\Property', 'xz', 'dm');
+	}
+
+	/**
+	 * 教学任务书
+	 * @author FuRongxin
+	 * @date    2016-03-13
+	 * @version 2.0
+	 * @return  object 所属对象
+	 */
+	public function task() {
+		return $this->belongsTo('App\Models\Task', 'kcxh', 'kcxh')
+			->whereNd($this->nd)
+			->whereXq($this->xq);
+	}
+
+	/**
+	 * 课程表
+	 * @author FuRongxin
+	 * @date    2016-02-24
+	 * @version 2.0
+	 * @return  object 所属对象
+	 */
+	public function timetables() {
+		return $this->hasMany('App\Models\Timetable', 'kcxh', 'kcxh')
+			->orderBy('zc');
+	}
+
+}
---
+++ app/Models/Timetable.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * 排课表
+ *
+ * @author FuRongxin
+ * @date 2016-03-13
+ * @version 2.0
+ */
+class Timetable extends Model {
+
+	protected $table = 'pk_kb';
+
+	protected $primaryKey = 'kcxh';
+
+	public $incrementing = false;
+
+	public $timestamps = false;
+
+	/**
+	 * 校区
+	 * @author FuRongxin
+	 * @date    2016-03-13
+	 * @version 2.0
+	 * @return  object 所属对象
+	 */
+	public function campus() {
+		return $this->belongsTo('App\Models\Campus', 'xqh', 'dm');
+	}
+
+	/**
+	 * 教室
+	 * @author FuRongxin
+	 * @date    2016-03-13
+	 * @version 2.0
+	 * @return  object 所属对象
+	 */
+	public function classroom() {
+		return $this->belongsTo('App\Models\Classroom', 'cdbh', 'jsh');
+	}
+
+	/**
+	 * 教师
+	 * @author FuRongxin
+	 * @date    2016-03-13
+	 * @version 2.0
+	 * @return  object 所属对象
+	 */
+	public function user() {
+		return $this->belongsTo('App\Models\User', 'jsgh', 'jsgh');
+	}
+
+	/**
+	 * 教学任务书
+	 * @author FuRongxin
+	 * @date    2016-02-24
+	 * @version 2.0
+	 * @return  object 所属对象
+	 */
+	public function task() {
+		return $this->belongsTo('App\Models\Task', 'kcxh', 'kcxh')
+			->whereNd($this->nd)
+			->whereXq($this->xq)
+			->whereId(1);
+	}
+
+	/**
+	 * 专业课程信息
+	 * @author FuRongxin
+	 * @date    2016-02-24
+	 * @version 2.0
+	 * @return  object 所属对象
+	 */
+	public function mjcourse() {
+		return $this->belongsTo('App\Models\Mjcourse', 'kcxh', 'kcxh')
+			->whereNd($this->nd)
+			->whereXq($this->xq)
+			->whereZsjj($this->zsjj);
+	}
+
+}
