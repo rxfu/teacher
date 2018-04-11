c9aa69c1907e2579d30bbd9e7ab8e10d819d3831
Stephen Fu <stephen.frx@gmail.com>
2018-04-11 12:38:26 +0800

    delete audit status

--- resources/views/dcxm/list.blade.php
+++ resources/views/dcxm/list.blade.php
@@ -17,7 +17,6 @@
                                 <th class="active">项目类别</th>
                                 <th class="active">所属学科</th>
                                 <th class="active">申请时间</th>
-                                <th class="active">审核状态</th>
                                 <th class="active">操作</th>
                             </tr>
                         </thead>
@@ -31,20 +30,11 @@
                                 <td>{{ $project->category->mc }}</td>
                                 <td>{{ $project->subject->mc }}</td>
                                 <td>{{ date('Y-m-d', strtotime($project->cjsj)) }}</td>
-                            	<td>
-                                    @if ($project->sfsh)
-                                        @if ($project->sftg)
-                                            审核已通过
-                                        @else
-                                            审核未通过
-                                        @endif
-                                    @else
-                                        未审核
-                                    @endif
-                                </td>
                                 <td>
                                     <a href="{{ url('dcxm/jsyj/' . $project->id) }}" title="填写教师意见" role="button" class="btn btn-success">填写教师意见</a>
-                                    <a href="{{ url('dcxm/pdf/' . $project->id) }}" title="下载申报书" role="button" class="btn btn-warning">下载申报书</a>
+                                    @if (!is_null($project->application))
+                                        <a href="{{ url('dcxm/pdf/' . $project->id) }}" title="下载申报书" role="button" class="btn btn-warning">下载申报书</a>
+                                    @endif
                                 </td>
                             </tr>
                             @endforeach
