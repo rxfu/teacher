b7b025449ecce1c5d608aa0bb29c5fa2f97c256d
Stephen Fu <stephen.frx@gmail.com>
2019-02-09 02:43:32 +0800

    fixed score import

--- app/Imports/StudentsImport.php
+++ app/Imports/StudentsImport.php
@@ -2,75 +2,63 @@
 
 namespace App\Imports;
 
-use App\Models\Ratio;
-use App\Models\Task;
-use Auth;
+use App\Models\Score;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 
-class StudentsImport implements ToModel
-{
+class StudentsImport implements ToCollection, WithStartRow {
+
 	private $kcxh;
 
-	public function __construct($kcxh) {
-		$this->kcxh=$kcxh
+	private $ratios;
+
+	public function __construct($kcxh, $ratios) {
+		$this->kcxh   = $kcxh;
+		$this->ratios = $ratios;
 	}
 
-    /**
-    * @param array $row
-    */
-    public function model(array $row)
-    {
-					$task = Task::whereKcxh($kcxh)
-						->whereNd(session('year'))
-						->whereXq(session('term'))
-						->whereJsgh(Auth::user()->jsgh)
-						->firstOrFail();
+	public function startRow(): int {
+		return 7;
+	}
 
-					$ratios = [];
-					$items  = Ratio::whereFs($task->cjfs)
-						->orderBy('id')
-						->get();
-					foreach ($items as $ratio) {
-						$ratios[] = [
-							'id'           => $ratio->id,
-							'name'         => $ratio->idm,
-							'value'        => $ratio->bl / $ratio->mf,
-							'allow_failed' => $ratio->jg,
-						];
-					}
+	/**
+	 * @param collection $row
+	 */
+	public function collection(Collection $rows) {
 
-					// 获取所有成绩
-					$results = $excel->skip(6)->get();
-					foreach ($results as $result) {
-						$student = Score::whereNd(session('year'))
-							->whereXq(session('term'))
-							->whereKcxh($kcxh)
-							->whereXh($result[0])
-							->firstOrFail();
+		// 获取所有成绩
+		foreach ($rows as $row) {
+			$row[0] = trim($row[0], "'");
 
-						foreach ($items as $item) {
-							$score = $result[$item->id + 1];
+			$student = Score::whereNd(session('year'))
+				->whereXq(session('term'))
+				->whereKcxh($this->kcxh)
+				->whereXh($row[0])
+				->firstOrFail();
 
-							if (100 < $score || 0 > $score) {
-								return $status = '学生' . $student->xh . '成绩有误，请检查后再重新导入';
-							} else {
-								$student->{'cj' . $item->id} = empty($score) ? 0 : $score;
-							}
-						}
+			foreach ($this->ratios as $item) {
+				$score = $row[$item['id'] + 1];
 
-						$total = 0;
-						$fails = [];
-						foreach ($ratios as $ratio) {
-							if (config('constants.score.passline') > $student->{'cj' . $ratio['id']} && config('constants.status.enable') == $ratio['allow_failed']) {
-								$fails[] = $student->{'cj' . $ratio['id']};
-							} else {
-								$total += $student->{'cj' . $ratio['id']} * $ratio['value'];
-							}
-						}
-						$student->zpcj = round(empty($fails) ? $total : min($fails));
+				if (100 < $score || 0 > $score) {
+					return $status = '学生' . $student->xh . '成绩有误，请检查后再重新导入';
+				} else {
+					$student->{'cj' . $item['id']} = empty($score) ? 0 : $score;
+				}
+			}
 
-return $student;
-}
-    }
+			$total = 0;
+			$fails = [];
+			foreach ($this->ratios as $ratio) {
+				if (config('constants.score.passline') > $student->{'cj' . $ratio['id']} && config('constants.status.enable') == $ratio['allow_failed']) {
+					$fails[] = $student->{'cj' . $ratio['id']};
+				} else {
+					$total += $student->{'cj' . $ratio['id']} * $ratio['value'];
+				}
+			}
+			$student->zpcj = round(empty($fails) ? $total : min($fails));
+
+			$student->save();
+		}
+	}
 }
