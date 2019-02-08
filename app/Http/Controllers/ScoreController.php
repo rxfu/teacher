741d243352fd3627531565d0984e395f4f69e5d1
Stephen Fu <stephen.frx@gmail.com>
2019-02-09 02:44:53 +0800

    delete some comments

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -444,64 +444,7 @@ class ScoreController extends Controller {
 						'allow_failed' => $ratio->jg,
 					];
 				}
-/*
-Excel::selectSheetsByIndex(0)->load(Storage::url('uploads/' . $filename), function ($excel) use ($kcxh, &$status) {
 
-$excel->noHeading();
-
-$task = Task::whereKcxh($kcxh)
-->whereNd(session('year'))
-->whereXq(session('term'))
-->whereJsgh(Auth::user()->jsgh)
-->firstOrFail();
-
-$ratios = [];
-$items  = Ratio::whereFs($task->cjfs)
-->orderBy('id')
-->get();
-foreach ($items as $ratio) {
-$ratios[] = [
-'id'           => $ratio->id,
-'name'         => $ratio->idm,
-'value'        => $ratio->bl / $ratio->mf,
-'allow_failed' => $ratio->jg,
-];
-}
-
-// 获取所有成绩
-$results = $excel->skip(6)->get();
-foreach ($results as $result) {
-$student = Score::whereNd(session('year'))
-->whereXq(session('term'))
-->whereKcxh($kcxh)
-->whereXh($result[0])
-->firstOrFail();
-
-foreach ($items as $item) {
-$score = $result[$item->id + 1];
-
-if (100 < $score || 0 > $score) {
-return $status = '学生' . $student->xh . '成绩有误，请检查后再重新导入';
-} else {
-$student->{'cj' . $item->id} = empty($score) ? 0 : $score;
-}
-}
-
-$total = 0;
-$fails = [];
-foreach ($ratios as $ratio) {
-if (config('constants.score.passline') > $student->{'cj' . $ratio['id']} && config('constants.status.enable') == $ratio['allow_failed']) {
-$fails[] = $student->{'cj' . $ratio['id']};
-} else {
-$total += $student->{'cj' . $ratio['id']} * $ratio['value'];
-}
-}
-$student->zpcj = round(empty($fails) ? $total : min($fails));
-
-$student->save();
-};
-}, 'UTF-8');
- */
 				Excel::import(new StudentsImport($kcxh, $ratios), $request->file('file'));
 
 				return redirect()->route('score.edit', $kcxh)->withStatus($status);
