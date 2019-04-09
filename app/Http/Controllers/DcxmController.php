ca02d0d85640a60b4776bc3a2beb87ed0543f7e4
Stephen Fu <stephen.frx@gmail.com>
2019-04-09 14:08:04 +0800

    fixed dcxmps

--- app/Http/Controllers/DcxmController.php
+++ app/Http/Controllers/DcxmController.php
@@ -19,199 +19,218 @@ use PDF;
  * @date 2018-02-03
  * @version 2.3
  */
-class DcxmController extends Controller {
+class DcxmController extends Controller
+{
 
-	/**
-	 * 显示大创项目列表
-	 *
-	 * @author FuRongxin
-	 * @date    2018-02-03
-	 * @version 2.3
-	 * @return  \Illuminate\Http\Response 大创项目列表
-	 */
-	public function getList() {
-		$projects = Dcxmxx::whereJsgh(Auth::user()->jsgh)
-			->orderBy('cjsj', 'desc')
-			->get();
-		$title = '项目列表';
+    /**
+     * 显示大创项目列表
+     *
+     * @author FuRongxin
+     * @date    2018-02-03
+     * @version 2.3
+     * @return  \Illuminate\Http\Response 大创项目列表
+     */
+    public function getList()
+    {
+        $projects = Dcxmxx::whereJsgh(Auth::user()->jsgh)
+            ->orderBy('cjsj', 'desc')
+            ->get();
+        $title = '项目列表';
 
-		return view('dcxm.list', compact('title', 'projects'));
-	}
+        return view('dcxm.list', compact('title', 'projects'));
+    }
 
-	/**
-	 * 大创项目教师意见
-	 *
-	 * @author FuRongxin
-	 * @date    2018-02-04
-	 * @version 2.3
-	 * @return  \Illuminate\Http\Response 大创项目基本信息
-	 */
-	public function getOpinion($id) {
-		$project = Dcxmxx::findOrFail($id);
-		$title   = '教师意见';
+    /**
+     * 大创项目教师意见
+     *
+     * @author FuRongxin
+     * @date    2018-02-04
+     * @version 2.3
+     * @return  \Illuminate\Http\Response 大创项目基本信息
+     */
+    public function getOpinion($id)
+    {
+        $project = Dcxmxx::findOrFail($id);
+        $title   = '教师意见';
 
-		return view('dcxm.opinion', compact('title', 'project'));
-	}
+        return view('dcxm.opinion', compact('title', 'project'));
+    }
 
-	/**
-	 * 保存大创项目教师意见
-	 *
-	 * @author FuRongxin
-	 * @date    2018-02-04
-	 * @version 2.3
-	 * @param   \Illuminate\Http\Request $request 教师意见请求
-	 * @return  \Illuminate\Http\Response 大创项目教师意见
-	 */
-	public function postOpinion(Request $request, $id) {
-		if ($request->isMethod('post')) {
-			$this->validate($request, [
-				'jsyj' => 'required|string',
-			]);
-			$inputs = $request->all();
+    /**
+     * 保存大创项目教师意见
+     *
+     * @author FuRongxin
+     * @date    2018-02-04
+     * @version 2.3
+     * @param   \Illuminate\Http\Request $request 教师意见请求
+     * @return  \Illuminate\Http\Response 大创项目教师意见
+     */
+    public function postOpinion(Request $request, $id)
+    {
+        if ($request->isMethod('post')) {
+            $this->validate($request, [
+                'jsyj' => 'required|string',
+            ]);
+            $inputs = $request->all();
 
-			$xmsq         = Dcxmsq::findOrFail($id);
-			$xmsq->jsyj   = $inputs['jsyj'];
-			$xmsq->jsyjsj = Carbon::now();
+            $xmsq         = Dcxmsq::findOrFail($id);
+            $xmsq->jsyj   = $inputs['jsyj'];
+            $xmsq->jsyjsj = Carbon::now();
 
-			$xmxx         = Dcxmxx::findOrFail($xmsq->xm_id);
-			$xmxx->jssfty = $inputs['jssfty'];
-			$xmxx->save();
+            $xmxx         = Dcxmxx::findOrFail($xmsq->xm_id);
+            $xmxx->jssfty = $inputs['jssfty'];
+            $xmxx->save();
 
-			if ($xmsq->save()) {
-				return redirect('dcxm/list')->withStatus('教师意见保存成功');
-			} else {
-				return back()->withErrors();
-			}
-		}
-	}
+            if ($xmsq->save()) {
+                return redirect('dcxm/list')->withStatus('教师意见保存成功');
+            } else {
+                return back()->withErrors();
+            }
+        }
+    }
 
-	/**
-	 * 下载证明材料
-	 * @author FuRongxin
-	 * @date    2018-01-18
-	 * @version 2.3
-	 * @return  \Illuminate\Http\Response 证明材料
-	 */
-	public function getFile($id) {
-		$xmsq     = Dcxmsq::findOrFail($id);
-		$filename = $xmsq->zmcl;
+    /**
+     * 下载证明材料
+     * @author FuRongxin
+     * @date    2018-01-18
+     * @version 2.3
+     * @return  \Illuminate\Http\Response 证明材料
+     */
+    public function getFile($id)
+    {
+        $xmsq     = Dcxmsq::findOrFail($id);
+        $filename = $xmsq->zmcl;
 
-		if (Storage::exists($filename)) {
-			$file = Storage::get($filename);
+        if (Storage::exists($filename)) {
+            $file = Storage::get($filename);
 
-			return response()->download(storage_path('uploads/' . $filename));
-		}
-	}
+            return response()->download(storage_path('uploads/' . $filename));
+        }
+    }
 
-	/**
-	 * 显示PDF申报书
-	 * @author FuRongxin
-	 * @date    2018-01-27
-	 * @version 2.3
-	 * @return  \Illuminate\Http\Response PDF申报书
-	 */
-	public function getPdf($id) {
-		$project = Dcxmxx::with('student', 'application', 'funds')->findOrFail($id);
-		$title   = '广西高校大学生创新创业计划项目申报书';
+    /**
+     * 显示PDF申报书
+     * @author FuRongxin
+     * @date    2018-01-27
+     * @version 2.3
+     * @return  \Illuminate\Http\Response PDF申报书
+     */
+    public function getPdf($id)
+    {
+        $project = Dcxmxx::with('student', 'application', 'funds')->findOrFail($id);
+        $title   = '广西高校大学生创新创业计划项目申报书';
 
-		return PDF::loadView('dcxm.pdf', compact('title', 'project'))
-			->setPaper('a4')
-			->setOption('margin-top', '3.7cm')
-			->setOption('margin-bottom', '3.5cm')
-			->setOption('margin-left', '2.8cm')
-			->setOption('margin-right', '2.6cm')
-			->inline($project->student->xh . '.pdf');
-	}
+        return PDF::loadView('dcxm.pdf', compact('title', 'project'))
+            ->setPaper('a4')
+            ->setOption('margin-top', '3.7cm')
+            ->setOption('margin-bottom', '3.5cm')
+            ->setOption('margin-left', '2.8cm')
+            ->setOption('margin-right', '2.6cm')
+            ->inline($project->student->xh . '.pdf');
+    }
 
-	/**
-	 * 获取PDF申报书
-	 * @author FuRongxin
-	 * @date    2018-01-27
-	 * @version 2.3
-	 * @return  \Illuminate\Http\Response PDF申报书
-	 */
-	public function getDownloadPdf($id) {
-		return PDF::loadFile(url('dcxm/pdf/' . $id))->download('application.pdf');
-	}
+    /**
+     * 获取PDF申报书
+     * @author FuRongxin
+     * @date    2018-01-27
+     * @version 2.3
+     * @return  \Illuminate\Http\Response PDF申报书
+     */
+    public function getDownloadPdf($id)
+    {
+        return PDF::loadFile(url('dcxm/pdf/' . $id))->download('application.pdf');
+    }
 
-	/**
-	 * 显示大创项目评审列表
-	 *
-	 * @author FuRongxin
-	 * @date    2019-02-18
-	 * @version 2.3
-	 * @return  \Illuminate\Http\Response 大创项目列表
-	 */
-	public function getPslist() {
-		if (Dcxmxt::find('PS_JB')->value == 0) {
-			$projects = Dcxmxx::whereJssfty(config('constants.status.enable'));
-		} elseif (Dcxmxt::find('PS_JB')->value == 1) {
-			$projects = Dcxmxx::wherexysfty(config('constants.status.enable'));
-		}
-		$projects = $projects->orderBy('cjsj', 'desc')->get();
-		$title    = '评审列表';
+    /**
+     * 显示大创项目评审列表
+     *
+     * @author FuRongxin
+     * @date    2019-02-18
+     * @version 2.3
+     * @return  \Illuminate\Http\Response 大创项目列表
+     */
+    public function getPslist()
+    {
+        $psjb = Dcxmxt::find('PS_JB')->value;
+        if ($psjb == 0) {
+            $projects = Dcxmxx::whereJssfty(config('constants.status.enable'));
+        } elseif ($psjb == 1) {
+            $projects = Dcxmxx::wherexysfty(config('constants.status.enable'));
+        }
 
-		return view('dcxm.pslist', compact('title', 'projects'));
-	}
+        $projects = $projects->with(['category', 'subject', 'reviews'])
+        ->whereHas('reviews', function ($query) use ($psjb) {
+            $query->whereZjgh(Auth::user()->jsgh)
+            ->wherePsjb($psjb);
+        })
+        ->orderBy('cjsj', 'desc')
+        ->get();
 
-	/**
-	 * 大创项目评审意见
-	 *
-	 * @author FuRongxin
-	 * @date    2018-02-188
-	 * @version 2.3
-	 * @return  \Illuminate\Http\Response 大创项目基本信息
-	 */
-	public function getXmps($id) {
-		$project = Dcxmxx::findOrFail($id);
-		$title   = '评审意见';
+        $title    = '评审列表';
 
-		if (Dcxmps::whereXmId($project->id)->whereZjgh(Auth::user()->jsgh)->exists()) {
-			$review = Dcxmps::whereXmId($project->id)
-				->whereZjgh(Auth::user()->jsgh)
-				->first();
-		} else {
-			$review = null;
-		}
+        return view('dcxm.pslist', compact('title', 'projects'));
+    }
 
-		return view('dcxm.xmps', compact('title', 'project', 'review'));
-	}
+    /**
+     * 大创项目评审意见
+     *
+     * @author FuRongxin
+     * @date    2018-02-188
+     * @version 2.3
+     * @return  \Illuminate\Http\Response 大创项目基本信息
+     */
+    public function getXmps($id)
+    {
+        $project = Dcxmxx::findOrFail($id);
+        $title   = '评审意见';
 
-	/**
-	 * 保存大创项目评审意见
-	 *
-	 * @author FuRongxin
-	 * @date    2019-02-18
-	 * @version 2.3
-	 * @param   \Illuminate\Http\Request $request 评审意见请求
-	 * @return  \Illuminate\Http\Response 大创项目评审意见
-	 */
-	public function postXmps(Request $request, $id) {
-		if ($request->isMethod('post')) {
-			$this->validate($request, [
-				'pf' => 'required|numeric|max:100|min:0',
-			]);
-			$inputs = $request->all();
+        if (Dcxmps::whereXmId($project->id)->whereZjgh(Auth::user()->jsgh)->exists()) {
+            $review = Dcxmps::whereXmId($project->id)
+                ->whereZjgh(Auth::user()->jsgh)
+                ->first();
+        } else {
+            $review = null;
+        }
 
-			if (Dcxmps::whereXmId($id)->whereZjgh(Auth::user()->jsgh)->exists()) {
-				$xmps = Dcxmps::whereXmId($id)
-					->whereZjgh(Auth::user()->jsgh)
-					->first();
-			} else {
-				$xmps = new Dcxmps;
-			}
-			$xmps->xm_id = $id;
-			$xmps->psyj  = $inputs['psyj'];
-			$xmps->pf    = $inputs['pf'];
-			$xmps->zjgh  = Auth::user()->jsgh;
-			$xmps->nd    = Carbon::now()->year;
-			$xmps->psjb  = Dcxmxt::find('PS_JB')->value;
+        return view('dcxm.xmps', compact('title', 'project', 'review'));
+    }
 
-			if ($xmps->save()) {
-				return redirect('dcxm/pslb')->withStatus('评审意见保存成功');
-			} else {
-				return back()->withErrors();
-			}
-		}
-	}
+    /**
+     * 保存大创项目评审意见
+     *
+     * @author FuRongxin
+     * @date    2019-02-18
+     * @version 2.3
+     * @param   \Illuminate\Http\Request $request 评审意见请求
+     * @return  \Illuminate\Http\Response 大创项目评审意见
+     */
+    public function postXmps(Request $request, $id)
+    {
+        if ($request->isMethod('post')) {
+            $this->validate($request, [
+                'pf' => 'required|numeric|max:100|min:0',
+            ]);
+            $inputs = $request->all();
+
+            if (Dcxmps::whereXmId($id)->whereZjgh(Auth::user()->jsgh)->exists()) {
+                $xmps = Dcxmps::whereXmId($id)
+                    ->whereZjgh(Auth::user()->jsgh)
+                    ->first();
+            } else {
+                $xmps = new Dcxmps;
+            }
+            $xmps->xm_id = $id;
+            $xmps->psyj  = $inputs['psyj'];
+            $xmps->pf    = $inputs['pf'];
+            $xmps->zjgh  = Auth::user()->jsgh;
+            $xmps->nd    = Carbon::now()->year;
+            $xmps->psjb  = Dcxmxt::find('PS_JB')->value;
+
+            if ($xmps->save()) {
+                return redirect('dcxm/pslb')->withStatus('评审意见保存成功');
+            } else {
+                return back()->withErrors();
+            }
+        }
+    }
 }
