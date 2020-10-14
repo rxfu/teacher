3d041a90e0499a716501b3a32258d12cdf1fe8ba
Stephen Fu <stephen.frx@gmail.com>
2020-10-14 23:42:27 +0800

    change back to cas

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,7 +58,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        // 'cas.auth'  => \App\Http\Middleware\CASAuth::class,
-        // 'cas.guest' => \Subfission\Cas\Middleware\RedirectCASAuthenticated::class,
+        'cas.auth'  => \App\Http\Middleware\CASAuth::class,
+        'cas.guest' => \Subfission\Cas\Middleware\RedirectCASAuthenticated::class,
     ];
 }
